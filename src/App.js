1a4a00e187ecc2c6a2a403178823c816109b52d6
Jonathan Villamizar <jvilla8a@gmail.com>
2020-09-23 00:03:55 -0500

    rebasing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
 import PokemonsDetails from "./pages/PokemonsDetails/PokemonDetails";
-=======
-import Home from "./pages/pokemons/Home";
->>>>>>> 564b947... fixing home
+import Home from "./pages/HomePage/Home";
 
 const App = () => (
   <Router>
     <Switch>
-<<<<<<< HEAD
-      <Route exact path="/" />
-<<<<<<< HEAD
-      <Route path="/pokemons/:id">
-        <PokemonsDetails />
-      </Route>
-=======
-      
->>>>>>> 35a15ef... crear home
-=======
       <Route exact path="/">
         <Home />
       </Route>
->>>>>>> 564b947... fixing home
+      <Route path="/pokemons/:id">
+        <PokemonsDetails />
+      </Route>
     </Switch>
   </Router>
 );
